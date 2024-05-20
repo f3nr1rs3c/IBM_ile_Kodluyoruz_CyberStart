b31a31f2f778c928017dbe695fea0a9ef4cc3cbc
Doğukan İSPİRLİ <51451598+f3nr1rs3c@users.noreply.github.com>
2024-05-20 22:53:08 +0300

    Create 8_comments.py

---
+++ IBM ile Kodluyoruz CyberStart - 🐍 Python 1/8_comments.py
@@ -0,0 +1,28 @@
+# Diyelim ki Bir kaç kişilik bir projede çalışıyorsunuz, programın farklı kısımlarını farklı insanlar yazıyor.
+
+# Başkası sizin yazdığınız kodu kullanmak istediğinde kodun ne yaptığını bilmesi gerekiyor. İşte burada "comment (yorum)" devreye giriyor.
+# Yazdığımız kodun ne yaptığını anlatan açıklamalar.
+
+# Yorumlar sadece insanların okuması için, program çalışırken burası program tarafından algılanmayacak
+
+# Sadece başkasının okuması da şart değil, seneler sonra yazdığınız kodu yeniden kullanmak isteyebilirsiniz ve neyin ne olduğunu, kodun ne yaptığını unutmuş olabilirsiniz.
+# Bu yüzden bu açıklamalar kendi kodunuzu okurken de önemli (seneler geçmesine de gerek yok, çok parçası olan bir problem üzerine çalışıyorsanız, şurada ne yapmıştım diye kendinizi bulmmanız çok muhtemel
+# o yüzdenb yorum eklemeyi alışkanlık haline getirmek gayet yararlı bir durum)
+
+# * Limon değişkeni satıaln limonları ifade ediyor.
+limon = 2
+
+# * sonuc değişkeni bir haftada kaç limon satıldığını belirtiyor.
+sonuc = limon * 7
+print(sonuc)
+
+# * Birden Çok Satırı Kapsayan Yorum Yazmak
+
+"""
+    Limon değişkeni satılan limonları ifade ediyor
+    
+    sonuc değişkeni bir haftada kaç limon satıldığını belirtiyor
+"""
+
+limon = 2
+sonuc = limon * 7
